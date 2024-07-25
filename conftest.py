837de7e9686ab6768d2fd7acd7d56400e77bbd2a
Svetlana Shakhova <SvetlaShakh@yandex.ru>
2024-07-25 15:55:42 +0300

    test_set_book_genre_not_from_books_genre_is_none change expected result collector_lst change number of books

--- conftest.py
+++ conftest.py
@@ -15,23 +15,13 @@ def collector():
 @pytest.fixture
 def collector_lst(collector):
     collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
-    collector.add_new_book('Гордость и предубеждение и зомби V2')
-    collector.set_book_genre('Гордость и предубеждение и зомби V2', 'Ужасы')
     collector.add_new_book('Что делать, если ваш кот хочет вас убить')
     collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Комедии')
-    collector.add_new_book('Что делать, если ваш кот V2')
-    collector.set_book_genre('Что делать, если ваш кот V2', 'Комедии')
     collector.add_new_book('38 попугаев')
     collector.set_book_genre('38 попугаев', 'Мультфильмы')
-    collector.add_new_book('38 попугаев V2')
-    collector.set_book_genre('38 попугаев V2', 'Мультфильмы')
     collector.add_new_book('Шерлок Холмс')
     collector.set_book_genre('Шерлок Холмс', 'Детективы')
-    collector.add_new_book('Шерлок Холмс V2')
-    collector.set_book_genre('Шерлок Холмс V2', 'Детективы')
     collector.add_new_book('351 градус по фарингейту')
     collector.set_book_genre('351 градус по фарингейту', 'Фантастика')
-    collector.add_new_book('351 градус по фарингейту V2')
-    collector.set_book_genre('351 градус по фарингейту V2', 'Фантастика')
 
     return collector
